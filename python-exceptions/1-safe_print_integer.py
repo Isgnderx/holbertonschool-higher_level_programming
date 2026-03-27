4744bd686c0095376cda3d6eed78cc21003ac78b
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-03-27 12:26:14 +0400

    Add

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 def safe_print_integer(value):
     try:
         if isinstance(value, int):
-            print(value)
+            print("{:d}".format(value))
             return True
     except:
         return False
