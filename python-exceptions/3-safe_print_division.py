1fab351063669efb410ec13ccc79c42914bab289
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-03-28 10:48:53 +0400

    aad

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
 
     try:
         result = a / b
-    except:
+    except Exception:
         result = None
     finally:
         print("Inside result: {}".format(result))
