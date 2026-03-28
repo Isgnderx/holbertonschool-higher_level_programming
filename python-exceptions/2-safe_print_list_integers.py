f917580116c9cb87c30eccfe8ef1bb197d33af79
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-03-28 10:14:22 +0400

    Faiq

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        print("{:d}".format(x))
-        return True
-    except:
-        return False
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except:
+            pass
+    print()
+    return count
