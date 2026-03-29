afb86cc6975f07d66a3169c0c3f02025436ececf
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-03-29 22:25:02 +0400

    Add

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+"""This module provides a function that adds two integers."""
+
 
 def add_integer(a, b=98):
-    sum = a + b
-    return sum
+    """Add two integers after casting float arguments to integers."""
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if isinstance(a, float):
+        a = int(a)
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b
