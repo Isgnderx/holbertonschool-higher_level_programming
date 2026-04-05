90882e92f1a9ecaf2432a2cced38fb0aa2332541
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-04-05 21:55:50 +0400

    ADDDDSUSYDU

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a Rectangle class."""
+
+
+class Rectangle:
+    """Rectangle class."""
+
+    def __init__(self):
+        """Initialize a new Rectangle instance."""
+        pass
+
+    @property
+    def dict_(self):
+        """Return an empty dictionary for testing purposes."""
+        return {}
