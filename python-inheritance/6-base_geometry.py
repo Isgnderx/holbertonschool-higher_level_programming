250bf2e1cdf1b68e3a00491ff413a75ff26ac53e
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-04-12 21:11:27 +0400

    ADD

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#6-base_geometry.py
+"""Andola"""
+
+class BaseGeometry():
+    """SALAM"""
+
+    def area(self):
+        """SALAM"""
+        raise Exception("area() is not implemented")
