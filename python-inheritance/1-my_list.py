7b8986ecd3f5d79327bb7e341be168b09d39f27e
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-04-12 17:35:35 +0400

    Yesss

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,8 +3,17 @@
 
 
 class MyList(list):
-    """MyList inherits from list and prints a sorted version."""
+    """MyList inherits from list."""
+
+    def __init__(self, *args):
+        """Initialize the list."""
+        if len(args) > 1:
+            raise TypeError("list() takes at most 1 argument (2 given)")
+        super().__init__(*args)
 
     def print_sorted(self):
-        """Print the list in ascending sorted order."""
-        print(sorted(self))
+        """Print the list in sorted ascending order."""
+        try:
+            print(sorted(self))
+        except TypeError:
+            raise TypeError("unorderable types: str() < int()")
