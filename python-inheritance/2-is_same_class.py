129a7ee754f48597622099618ebfabf4997f4c63
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-04-12 17:55:00 +0400

    ADd

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+# 2-is_same_class.py
+"""Izah"""
+
 
 def is_same_class(obj, a_class):
+    """ANDOLA Izah"""
     if type(obj) == a_class:
         return True
     return False
