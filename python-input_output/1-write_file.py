e7843450cdfa9357be546be7b1a9c78d841782e9
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-04-15 11:36:07 +0400

    izah

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""Defines a function that writes a 
-string to a text file (UTF8) and returns 
-the number of characters written."""
+"""izah"""
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns 
-    the number of characters written."""
+    """izah."""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
