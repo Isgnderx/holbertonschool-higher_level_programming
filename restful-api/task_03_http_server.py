46399b5ba0caf343217b830a5fbb0d9ee999c8d3
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-04-26 22:50:54 +0400

    salam

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+class SimpleAPIHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("Hello, this is a simple API!".encode("utf-8"))
+
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+
+if __name__ == "__main__":
+    server = HTTPServer(("localhost", 8000), SimpleAPIHandler)
+    print("Server running on http://localhost:8000")
+    server.serve_forever()
